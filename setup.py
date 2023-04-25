66bd5899577f6ec50fe810d779869b9ac6c77461
Abhigyan-Mishra <mishra.abhigyan@hotmail.com>
2023-04-25 16:36:40 +0530

    Random BS go brrrr

--- setup.py
+++ setup.py
@@ -8,16 +8,16 @@ setup(
     version='0.1.5',
     description="General use optimizer for non-convex cost\
                 functions with non-linear constraints",
-    author='Fernando Wanguemert',
+    author='Abhigyan Mishra',
     license_files=('LICENSE',),
     long_description=long_description,
-    url='https://github.com/ferwanguer/PyQEA',
+    url='https://github.com/Abhigyan-Mishra/Quantum-Inspired-Sampling-Based-Optimization',
     long_description_content_type="text/markdown",
-    author_email='f.wguerra@outlook.com',
+    author_email='abhigyanm5000@gmail.com',
     packages=find_packages(),  # same as name
     classifiers=[
                  "Programming Language :: Python :: 3",
-                 "License :: OSI Approved :: MIT License",
+                 "License :: OSI Approved :: wtfpl License",
                  "Operating System :: OS Independent",
     ],
     install_requires=[
